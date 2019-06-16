ea72c9d0457351aeed0133ecace9761984fb626b
adj-dev <andrew.johnson2073@gmail.com>
2019-06-16 11:16:40 -0500

    added comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,15 @@
+// Dependencies
 const express = require('express');
 const router = express.Router();
 
 
 
+// Define all routes
 router.get('/', (req, res) => {
   res.render('home');
 });
 
 
 
+// Export the router as a module
 module.exports = router;
