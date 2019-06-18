bcdbecf0f36299eac4995783558303e340c170b9
adj-dev <andrew.johnson2073@gmail.com>
2019-06-17 20:15:52 -0500

    Added a hide/show button

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # web-scraper
-A simple web scraper app. 
+A simple web scraper app that uses **cheerio**, **mongoose**, and **axios**. 
+
+## How to Use
+
+
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
 $(function () {
   // Add an event listener for the `scraper` button
   $(document).on('click', '#scraper', function () {
-    console.log('clicked the scraper button');
     $.ajax({
       method: 'GET',
       url: '/scrape'
@@ -10,5 +9,28 @@ $(function () {
         console.log(res);
         window.location.reload(true);
       });
+  });
+
+
+
+  // Add an event listener for the `clear` button
+  $(document).on('click', '#toggleDisplay', function () {
+    // Capture the `data-state` attribute from the target element
+    let state = $(this).attr('data-state');
+
+    // If `state` === 'shown' hide the container div, otherwise show the container div
+    if (state === 'shown') {
+      $('.container').hide();
+
+      // Toggle the appearance and attribute values of the button
+      $(this).text('Show');
+      $(this).attr('data-state', 'hidden');
+    } else if (state === 'hidden') {
+      $('.container').show();
+
+      // Toggle the appearance and attribute values of the button
+      $(this).text('Hide');
+      $(this).attr('data-state', 'shown');
+    }
   });
 });
